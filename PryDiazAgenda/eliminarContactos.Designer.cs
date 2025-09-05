d4a403da5a40f72b4da96907c5c7ced0bfb28e9a
agusdiaz1 <agusssd73@gmail.com>
2025-09-05 18:11:16 -0300

    conexion de la base de datos y parte del codigo

--- PryDiazAgenda/eliminarContactos.Designer.cs
+++ PryDiazAgenda/eliminarContactos.Designer.cs
@@ -28,41 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(eliminarContactos));
             this.panel1 = new System.Windows.Forms.Panel();
             this.lblID = new System.Windows.Forms.Label();
             this.txtId = new System.Windows.Forms.TextBox();
             this.btnEliminar = new System.Windows.Forms.Button();
+            this.dgvContactos = new System.Windows.Forms.DataGridView();
             this.trvContactos = new System.Windows.Forms.TreeView();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.colID = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colCategoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colApellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colCorreo = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colTelefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.lblContacto = new System.Windows.Forms.Label();
+            this.lblContactos = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContactos)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.lblContacto);
-            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.lblContactos);
             this.panel1.Controls.Add(this.trvContactos);
+            this.panel1.Controls.Add(this.dgvContactos);
             this.panel1.Controls.Add(this.btnEliminar);
             this.panel1.Controls.Add(this.lblID);
             this.panel1.Controls.Add(this.txtId);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(645, 415);
+            this.panel1.Size = new System.Drawing.Size(607, 426);
             this.panel1.TabIndex = 0;
             // 
             // lblID
             // 
             this.lblID.AutoSize = true;
             this.lblID.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblID.Location = new System.Drawing.Point(305, 24);
+            this.lblID.Location = new System.Drawing.Point(271, 16);
             this.lblID.Name = "lblID";
             this.lblID.Size = new System.Drawing.Size(22, 17);
             this.lblID.TabIndex = 9;
@@ -70,99 +65,61 @@
             // 
             // txtId
             // 
-            this.txtId.Location = new System.Drawing.Point(347, 24);
+            this.txtId.Location = new System.Drawing.Point(310, 16);
             this.txtId.Name = "txtId";
-            this.txtId.Size = new System.Drawing.Size(57, 20);
+            this.txtId.Size = new System.Drawing.Size(55, 20);
             this.txtId.TabIndex = 10;
             // 
             // btnEliminar
             // 
             this.btnEliminar.BackColor = System.Drawing.Color.MistyRose;
             this.btnEliminar.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEliminar.Location = new System.Drawing.Point(427, 14);
+            this.btnEliminar.Location = new System.Drawing.Point(394, 6);
             this.btnEliminar.Name = "btnEliminar";
             this.btnEliminar.Size = new System.Drawing.Size(198, 34);
             this.btnEliminar.TabIndex = 15;
             this.btnEliminar.Text = "Eliminar contacto";
             this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // dgvContactos
+            // 
+            this.dgvContactos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvContactos.Location = new System.Drawing.Point(264, 57);
+            this.dgvContactos.Name = "dgvContactos";
+            this.dgvContactos.Size = new System.Drawing.Size(328, 352);
+            this.dgvContactos.TabIndex = 16;
             // 
             // trvContactos
             // 
-            this.trvContactos.Location = new System.Drawing.Point(16, 24);
+            this.trvContactos.Location = new System.Drawing.Point(14, 31);
             this.trvContactos.Name = "trvContactos";
-            this.trvContactos.Size = new System.Drawing.Size(240, 378);
-            this.trvContactos.TabIndex = 16;
+            this.trvContactos.Size = new System.Drawing.Size(241, 378);
+            this.trvContactos.TabIndex = 17;
             // 
-            // dataGridView1
+            // lblContactos
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.colID,
-            this.colCategoria,
-            this.colNombre,
-            this.colApellido,
-            this.colCorreo,
-            this.colTelefono});
-            this.dataGridView1.Location = new System.Drawing.Point(278, 67);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(347, 335);
-            this.dataGridView1.TabIndex = 17;
-            // 
-            // colID
-            // 
-            this.colID.HeaderText = "ID";
-            this.colID.Name = "colID";
-            this.colID.Width = 60;
-            // 
-            // colCategoria
-            // 
-            this.colCategoria.HeaderText = "Categoria";
-            this.colCategoria.Name = "colCategoria";
-            this.colCategoria.Width = 80;
-            // 
-            // colNombre
-            // 
-            this.colNombre.HeaderText = "Nombre";
-            this.colNombre.Name = "colNombre";
-            // 
-            // colApellido
-            // 
-            this.colApellido.HeaderText = "Apellido";
-            this.colApellido.Name = "colApellido";
-            // 
-            // colCorreo
-            // 
-            this.colCorreo.HeaderText = "Correo";
-            this.colCorreo.Name = "colCorreo";
-            // 
-            // colTelefono
-            // 
-            this.colTelefono.HeaderText = "Telefono";
-            this.colTelefono.Name = "colTelefono";
-            this.colTelefono.Width = 80;
-            // 
-            // lblContacto
-            // 
-            this.lblContacto.AutoSize = true;
-            this.lblContacto.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblContacto.Location = new System.Drawing.Point(13, 0);
-            this.lblContacto.Name = "lblContacto";
-            this.lblContacto.Size = new System.Drawing.Size(83, 21);
-            this.lblContacto.TabIndex = 18;
-            this.lblContacto.Text = "Contactos";
+            this.lblContactos.AutoSize = true;
+            this.lblContactos.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContactos.Location = new System.Drawing.Point(11, 0);
+            this.lblContactos.Name = "lblContactos";
+            this.lblContactos.Size = new System.Drawing.Size(83, 21);
+            this.lblContactos.TabIndex = 18;
+            this.lblContactos.Text = "Contactos";
             // 
             // eliminarContactos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.RosyBrown;
-            this.ClientSize = new System.Drawing.Size(671, 442);
+            this.ClientSize = new System.Drawing.Size(633, 450);
             this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "eliminarContactos";
             this.Text = "Eliminar Contactos";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContactos)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -172,15 +129,9 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label lblID;
         private System.Windows.Forms.TextBox txtId;
-        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label lblContactos;
         private System.Windows.Forms.TreeView trvContactos;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn colID;
-        private System.Windows.Forms.DataGridViewTextBoxColumn colCategoria;
-        private System.Windows.Forms.DataGridViewTextBoxColumn colNombre;
-        private System.Windows.Forms.DataGridViewTextBoxColumn colApellido;
-        private System.Windows.Forms.DataGridViewTextBoxColumn colCorreo;
-        private System.Windows.Forms.DataGridViewTextBoxColumn colTelefono;
-        private System.Windows.Forms.Label lblContacto;
+        private System.Windows.Forms.DataGridView dgvContactos;
+        private System.Windows.Forms.Button btnEliminar;
     }
 }
